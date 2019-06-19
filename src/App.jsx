e849f45bb809a1f7b7cad864ce59d352e83a4130
bobmwangih <bobbymwangih@gmail.com>
2019-06-19 12:38:30 +0300

    using webpack manually-bundling up app.js and issueAdd.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 const contentNode = document.getElementById("contents");
-//const MongoClient=require('mongodb').MongoClient;
+import IssueAdd from './IssueAdd.js';
+
 
 
 
@@ -56,36 +57,6 @@ class IssueTable extends React.Component {
 class IssueFilter extends React.Component {
     render() {
         return <div>This is a placeholder for the Issue Filter.</div>;
-    }
-}
-
-class IssueAdd extends React.Component {
-    constructor() {
-        super();
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
-    handleSubmit(e) {
-        e.preventDefault();
-        var form = document.forms.issueAdd;
-        this.props.createIssue({
-            owner: form.owner.value,
-            title: form.title.value,
-            status: 'New',
-            created: new Date(),
-        });
-        // clear the form for the next input
-        form.owner.value = ""; form.title.value = "";
-    }
-    render() {
-        return (
-            <div>
-                <form name="issueAdd" onSubmit={this.handleSubmit}>
-                    <input type="text" name="owner" placeholder="Owner" />
-                    <input type="text" name="title" placeholder="Title" />
-                    <button>Add</button>
-                </form>
-            </div>
-        )
     }
 }
 
